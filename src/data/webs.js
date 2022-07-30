6a5f737305a621adbff9f0c094e0ce6fa68929b3
Pablo Fabian <pablo.eth@hotmail.com>
2022-07-30 04:57:00 -0500

    add: data

---
+++ src/data/webs.js
@@ -0,0 +1,362 @@
+export const data = [
+  {
+    name: 'Does it mutate?',
+    url: 'https://doesitmutate.xyz/',
+    description: 'Métodos de JavaScript que mutan o no el objeto original',
+    categories: ['javascript']
+  },
+  {
+    name: 'Bundlephobia',
+    url: 'https://bundlephobia.com/',
+    description: 'Encuentra el costo de agregar una dependencia a tu proyecto',
+    categories: ['utilities']
+  },
+  {
+    name: 'Lorca Editor',
+    url: 'https://lorcaeditor.com/',
+    description: 'Corrector gramatical, ortográfico y de estilo',
+    categories: ['utilities']
+  },
+  {
+    name: 'Squoosh',
+    url: 'https://squoosh.app/',
+    description: 'Reduce el tamaño de tus imágenes sin perder calidad',
+    categories: ['utilities', 'images']
+  },
+  {
+    name: 'Epoch Converter',
+    url: 'https://www.epochconverter.com/',
+    description: 'Conversión de marcas de tiempo de Epoch y Unix',
+    categories: ['utilities']
+  },
+  {
+    name: 'Carbon',
+    url: 'https://carbon.now.sh/',
+    description: 'Crea y comparte imágenes de tu código fuente',
+    categories: ['utilities']
+  },
+  {
+    name: 'HTTP Cats',
+    url: 'https://http.cat/',
+    description: 'Conoce los códigos de estado HTTP con imágenes de gatos',
+    categories: ['learning']
+  },
+  {
+    name: 'Javascript Info',
+    url: 'https://es.javascript.info/',
+    description: 'Temas básicos y avanzados de JavaScript en español',
+    categories: ['learning']
+  },
+  {
+    name: 'Fullstack Open',
+    url: 'https://fullstackopen.com/es/',
+    description: 'Aprenda React, Redux, Node.js, MongoDB, GraphQL y TypeScript de una sola vez',
+    categories: ['learning']
+  },
+  {
+    name: 'Frontend Mentor',
+    url: 'https://www.frontendmentor.io/',
+    description: 'Resuelve desafíos de diseño y código para mejorar tus habilidades',
+    categories: ['challenges']
+  },
+  {
+    name: 'Learn Anything',
+    url: 'https://learn-anything.xyz/',
+    description: 'Recursos para aprender cualquier tema según lo más votado por la comunidad',
+    categories: ['learning']
+  },
+  {
+    name: 'Dev Challenges',
+    url: 'https://devchallenges.io/',
+    description: 'Desafíos de desarrollo web para mejorar tus habilidades',
+    categories: ['challenges']
+  },
+  {
+    name: 'Bashrc Generator',
+    url: 'https://bashrcgenerator.com/',
+    description: 'Genera tu archivo .bashrc fácilmente con una interfaz de arrastrar y soltar',
+    categories: ['personalization']
+  },
+  {
+    name: 'Typing Club',
+    url: 'https://www.typingclub.com/',
+    description: 'Aprende a escribir más rápido con ejercicios de práctica',
+    categories: ['learning']
+  },
+  {
+    name: 'Free for Developers',
+    url: 'https://free-for.dev/',
+    description: 'Servicios con planes gratuitos para desarrolladores',
+    categories: ['resources']
+  },
+  {
+    name: 'Open Doodles',
+    url: 'https://www.opendoodles.com/',
+    description: 'Un conjunto gratuito de ilustraciones de código abierto',
+    categories: ['resources']
+  },
+  {
+    name: 'Mockaroo',
+    url: 'https://www.mockaroo.com/',
+    description: 'Genera datos falsos para tus pruebas y prototipos',
+    categories: ['utilities']
+  },
+  {
+    name: 'Iconoir',
+    url: 'https://iconoir.com/',
+    description: 'Un conjunto gratuito de iconos de código abierto',
+    categories: ['resources', 'icons']
+  },
+  {
+    name: 'Open Graph Image',
+    url: 'https://og-image.vercel.app/',
+    description: 'Genera imágenes de Open Graph para tus proyectos',
+    categories: ['utilities', 'images']
+  },
+  {
+    name: 'SVGR',
+    url: 'https://react-svgr.com/',
+    description: 'Convierte SVG en componentes de React',
+    categories: ['utilities']
+  },
+  {
+    name: 'Feather',
+    url: 'https://feathericons.com/',
+    description: 'Iconos de código abierto simplemente hermosos',
+    categories: ['resources', 'icons']
+  },
+  {
+    name: 'Exercism',
+    url: 'https://exercism.org/',
+    description: 'Mejora tu programación a través de divertidos ejercicios de codificación',
+    categories: ['challenges']
+  },
+  {
+    name: 'JavaScript Utilities',
+    url: 'https://1loc.dev/',
+    description: 'Utilidades de JavaScript en una línea de código',
+    categories: ['javascript']
+  },
+  {
+    name: 'JavaScript Resources',
+    url: 'https://www.java5cript.com/',
+    description: 'Aprende JavaScript a través de cursos, tutoriales, videos y más',
+    categories: ['learning']
+  },
+  {
+    name: 'CSS Gradient Generator',
+    url: 'https://www.css-gradient.com/',
+    description: 'Genera un fondo degradado colorido para tus diseños',
+    categories: ['utilities', 'css']
+  },
+  {
+    name: 'Heroicons',
+    url: 'https://heroicons.com/',
+    description: 'Iconos hechos a mano por los creadores de Tailwind CSS',
+    categories: ['resources', 'icons']
+  },
+  {
+    name: 'Bash Prompt Generator',
+    url: 'https://scriptim.github.io/bash-prompt-generator/',
+    description: 'Crea tu propio indicador de línea de comandos personalizado',
+    categories: ['personalization']
+  },
+  {
+    name: 'Can I Email',
+    url: 'https://www.caniemail.com/',
+    description: 'Descubre si puedes usar una característica de HTML o CSS en un correo electrónico',
+    categories: ['utilities']
+  },
+  {
+    name: 'Web Code Tools',
+    url: 'https://webcode.tools/',
+    description: 'Generadores de código para desarrollo front-end',
+    categories: ['utilities']
+  },
+  {
+    name: 'Animations',
+    url: 'https://css-animations.io/',
+    description: 'Aprende a crear animaciones con CSS',
+    categories: ['learning', 'css']
+  },
+  {
+    name: 'Animista',
+    url: 'https://animista.net/',
+    description: 'Biblioteca de animaciones CSS',
+    categories: ['utilities', 'css']
+  },
+  {
+    name: 'Gradientos',
+    url: 'https://www.gradientos.app/',
+    description: 'Gradientes aplicados en elementos comunes de interfaz de usuario',
+    categories: ['utilities', 'css']
+  },
+  {
+    name: 'Wicked Backgrounds',
+    url: 'https://wickedbackgrounds.com/',
+    description: 'Crea ondas de fondo para tus diseños',
+    categories: ['utilities']
+  },
+  {
+    name: 'Slidev',
+    url: 'https://sli.dev/',
+    description: 'Crea presentaciones de diapositivas con Markdown y Vue',
+    categories: ['utilities']
+  },
+  {
+    name: 'Easings',
+    url: 'https://easings.co/',
+    description: 'Prueba curvas de aceleración comunes en una variedad de interfaces',
+    categories: ['utilities', 'css']
+  },
+  {
+    name: 'Web Learn',
+    url: 'https://web.dev/learn/',
+    description: 'Explora cursos de diseño y desarrollo web',
+    categories: ['learning']
+  },
+  {
+    name: 'Compress Image',
+    url: 'https://compressimage.io/',
+    description: 'Comprime imágenes sin pérdida de calidad',
+    categories: ['utilities', 'images']
+  },
+  {
+    name: 'Can I Use',
+    url: 'https://caniuse.com/',
+    description: 'Descubre si puedes usar una característica de HTML o CSS en un navegador',
+    categories: ['utilities']
+  },
+  {
+    name: 'System UIcons',
+    url: 'https://systemuicons.com/',
+    description: 'Una colección de íconos simples y consistentes para sistemas y productos',
+    categories: ['resources', 'icons']
+  },
+  {
+    name: 'Advent JS',
+    url: 'https://adventjs.dev/',
+    description: 'Prueba tus habilidades de JavaScript con 24 desafíos de programación',
+    categories: ['challenges']
+  },
+  {
+    name: 'Meta Tags',
+    url: 'https://metatags.io/',
+    description: 'Genera metadatos para tus sitios web',
+    categories: ['utilities']
+  },
+  {
+    name: 'Leet Code',
+    url: 'https://leetcode.com/',
+    description: 'Mejora tus habilidades y prepárate para entrevistas técnicas',
+    categories: ['challenges']
+  },
+  {
+    name: 'Hacker Rank',
+    url: 'https://www.hackerrank.com/',
+    description: 'Explora y amplía tus habilidades para entrevistas técnicas',
+    categories: ['challenges']
+  },
+  {
+    name: 'Loaders',
+    url: 'https://cssloaders.github.io/',
+    description: 'Biblioteca de cargadores y spinners CSS',
+    categories: ['resources', 'css']
+  },
+  {
+    name: 'Dev Docs',
+    url: 'https://devdocs.io/',
+    description: 'Aprende y consulta documentación de tecnologías web',
+    categories: ['learning']
+  },
+  {
+    name: 'Programming Fonts',
+    url: 'https://www.programmingfonts.org/',
+    description: 'Prueba fuentes de programación para mejorar tu flujo de trabajo',
+    categories: ['personalization']
+  },
+  {
+    name: 'CSS Battle',
+    url: 'https://cssbattle.dev/',
+    description: 'Usa tus habilidades de CSS para replicar objetos con el código más pequeño posible',
+    categories: ['challenges', 'css']
+  },
+  {
+    name: 'Free Programming Books',
+    url: 'https://ebookfoundation.github.io/',
+    description: 'Encuentra libros de programación gratuitos',
+    categories: ['learning']
+  },
+  {
+    name: 'Rapid API',
+    url: 'https://rapidapi.com/',
+    description: 'Encuentra y consume APIs de terceros',
+    categories: ['utilities']
+  },
+  {
+    name: 'Transform Tools',
+    url: 'https://transform.tools/',
+    description: 'Una lista de herramientas para transformar código',
+    categories: ['utilities']
+  },
+  {
+    name: 'First Aid Git',
+    url: 'https://firstaidgit.io/',
+    description: 'Una colección de las preguntas de Git más frecuentes',
+    categories: ['learning']
+  },
+  {
+    name: 'Blob Animation',
+    url: 'https://blobanimation.com/',
+    description: 'Genera blobs animados suaves',
+    categories: ['utilities', 'css']
+  },
+  {
+    name: 'APIs List',
+    url: 'https://apislist.com/',
+    description: 'Una lista seleccionada de APIs públicas',
+    categories: ['utilities']
+  },
+  {
+    name: 'Omatsuri',
+    url: 'https://omatsuri.app/',
+    description: 'Herramientas de navegador de código abierto para el uso diario',
+    categories: ['utilities']
+  },
+  {
+    name: 'Web Gradients',
+    url: 'https://webgradients.com/',
+    description: 'Colección gratuita de degradados lineales',
+    categories: ['utilities', 'css']
+  },
+  {
+    name: 'Can Include',
+    url: 'https://caninclude.glitch.me/',
+    description: 'Descubre si puedes usar una etiqueta HTML en un elemento',
+    categories: ['utilities']
+  },
+  {
+    name: 'Ray',
+    url: 'https://ray.so/',
+    description: 'Crea y comparte capturas de pantalla de código',
+    categories: ['utilities']
+  },
+  {
+    name: 'JSON Formatter',
+    url: 'https://jsonformatter.curiousconcept.com/',
+    description: 'Formatea y valida JSON',
+    categories: ['utilities']
+  },
+  {
+    name: 'Lucide',
+    url: 'https://lucide.dev/',
+    description: 'Una bifurcación de Feather Icons con más de 800 íconos',
+    categories: ['resources', 'icons']
+  },
+  {
+    name: 'Tabler Icons',
+    url: 'https://tabler-icons.io/',
+    description: 'Íconos de píxeles perfectos para diseño web',
+    categories: ['resources', 'icons']
+  }
+]
