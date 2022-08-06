249e8bcd929fce6bbe0bbcb933a933542fe13066
Pablo Fabian <pablo.eth@hotmail.com>
2022-08-06 04:37:00 -0500

    add: search icon

---
+++ src/icons/Search.jsx
@@ -0,0 +1,24 @@
+export const SearchIcon = (props) => (
+  <svg
+    width={34}
+    height={34}
+    strokeWidth={1}
+    viewBox='0 0 24 24'
+    fill='none'
+    xmlns='http://www.w3.org/2000/svg'
+    {...props}
+  >
+    <path
+      d='M15.5 15.5L19 19'
+      stroke='currentColor'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+    <path
+      d='M5 11C5 14.3137 7.68629 17 11 17C12.6597 17 14.1621 16.3261 15.2483 15.237C16.3308 14.1517 17 12.654 17 11C17 7.68629 14.3137 5 11 5C7.68629 5 5 7.68629 5 11Z'
+      stroke='currentColor'
+      strokeLinecap='round'
+      strokeLinejoin='round'
+    />
+  </svg>
+)
